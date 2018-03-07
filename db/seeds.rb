b5a0a76d51d9439b64a25b1ebb82f35ef4235276
jonathan joubert <jjoubert@mandarine-gestion.com>
2018-03-07 15:35:00 +0100

    exo1 all roads

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Jules Vernes',
+    address:      'Tour Eiffel 1er etage, Paris',
+    phone_number: '0145673290',
+    category:     'french'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '00443642476',
+    category:      'italian'
+  },
+  {
+    name:         'Pink Mamma',
+    address:      '20bis Rue de Douai, 75009 Paris',
+    phone_number: '0145622210',
+    category:     'italian'
+  },
+  {
+    name:         'Alcazar',
+    address:      '62 Rue Mazarine, 75006 Paris',
+    phone_number:  '0153101999',
+    category:      'french'
+  },
+  {
+    name:         'Pizza South',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '00443642476',
+    category:      'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
